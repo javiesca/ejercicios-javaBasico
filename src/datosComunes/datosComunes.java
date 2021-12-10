bd20a6fcf08872c03ba0a43a619fb54ef3b1d867
javiesca <viesca@gmail.com>
2021-12-10 22:17:58 +0100

    Ejercicios Java Básico

---
+++ src/datosComunes/datosComunes.java
@@ -0,0 +1,37 @@
+package datosComunes;
+
+public class datosComunes {
+
+    public static void main(String[] args) {
+
+
+        // Numéricos
+        byte num1 = 1;
+        short num2 = 25;
+        int num3 = 371647832;
+        long num4 = 349873429545645L;
+        float num5 = 34.232445562f;
+        double num6 = 32.54564456465465546d;
+
+        // Booleano
+
+        boolean part1 = true;
+
+
+        // Caracteres
+
+        char caracter = 'a';
+        String frase = "Hola que tal?";
+
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(part1);
+        System.out.println(caracter);
+        System.out.println(frase);
+    }
+}
