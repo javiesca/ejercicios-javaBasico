a27525ebc1f44d74542735e14b2eb9e4196a8a06
javiesca <viesca@gmail.com>
2021-12-10 22:21:47 +0100

    Ejercicios Java Básico

---
+++ src/funcionIVA/funcionIVA.java
@@ -0,0 +1,15 @@
+package funcionIVA;
+
+public class funcionIVA {
+
+    public static void main(String[] args) {
+
+        System.out.println(precioIVA(100));
+
+    }
+
+    public static double precioIVA(double precio){
+
+        return precio * 0.21 + precio;
+    }
+}
