9c63859b1436773a333722e5557cf1bd176f0fe8
javiesca <viesca@gmail.com>
2021-12-11 19:16:02 +0100

    Ejercicios Java Básico

--- src/ejercicio4_POO_SmartDevice/Main.java
+++ src/ejercicio4_POO_SmartDevice/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-
         SmartPhone iphone = new SmartPhone("Apple", "iPhone", 1800, 900,
                 "OLED", 7, true,24);
 
         System.out.println(iphone);
 
+
         System.out.println("");
+
+
 
         SmartWatch galaxy = new SmartWatch("Samsung", "Galaxy Watch", 600, 445, "LCD", true,
                 false, true);
